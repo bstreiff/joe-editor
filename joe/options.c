bstreiff/joe-editor

commit f654dad1e0715e199945b4c1c7dbc5136dc92f69

don't use realpath
realpath() returns NULL if the file doesn't exist, which can be the
case if we're creating a new file. Instead, split the codepath into
"absolute path" and "construct an absolute path from the working
directory" cases.

Fixes: 046cd0f849a1 ("editorconfig support for indentation options")

joe/options.c
@@ -255,12 +255,21 @@ static void setopt_editorconfig(B *b)
 	if (!b->name || !zcmp(b->name, "* Startup Log *"))
 		return;
 
-	/* get canonical pathname; editorconfig requires the absolute path */
-	fullpath = realpath(b->name, NULL);
-
 	ech = editorconfig_handle_init();
 
-	ecerr = editorconfig_parse(fullpath, ech);
+	/* editorconfig requires an absolute path */
+	if (b->name[0] == '/') {
+		ecerr = editorconfig_parse(b->name, ech);
+	} else {
+		n = strlen(pwd()) + strlen(b->name) + 1;
+		fullpath = joe_malloc(n);
+		if (!fullpath)
+			return;
+		joe_snprintf_2(fullpath, n, "%s/%s", pwd(), b->name);
+		ecerr = editorconfig_parse(fullpath, ech);
+		joe_free(fullpath);
+	}
+
 	if (ecerr < 0) {
 		const char *msg = editorconfig_get_error_msg(ecerr);
 		logerror_2("error %d from editorconfig: %s\n", ecerr, msg);
@@ -297,7 +306,6 @@ static void setopt_editorconfig(B *b)
 
 	done:
 	editorconfig_handle_destroy(ech);
-	free(fullpath);
 }
 #else
 static void setopt_editorconfig(B *)

commit 0862c175c219b7950cc597cae86c9370f7f675dd

create absolute path buffer long enough for slash and null terminator
When concatenating pwd() and the filename, we created a buffer that was
one character longer for the null terminator, but we weren't also
accounting for the additional "/" as the joiner, so the full path that
resulted would have its last character truncated.

Fixes: f654dad1e071 ("don't use realpath")

joe/options.c
@@ -261,7 +261,7 @@ static void setopt_editorconfig(B *b)
 	if (b->name[0] == '/') {
 		ecerr = editorconfig_parse(b->name, ech);
 	} else {
-		n = strlen(pwd()) + strlen(b->name) + 1;
+		n = strlen(pwd()) + strlen("/") + strlen(b->name) + 1;
 		fullpath = joe_malloc(n);
 		if (!fullpath)
 			return;